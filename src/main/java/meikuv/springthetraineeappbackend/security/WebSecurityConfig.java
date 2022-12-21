a8b9a20864c0d93b09a83df0b7c9232196968bd3
meikuv <mega.kulmanov@list.ru>
2022-12-21 22:18:33 +0600

    fix

--- src/main/java/meikuv/springthetraineeappbackend/security/WebSecurityConfig.java
+++ src/main/java/meikuv/springthetraineeappbackend/security/WebSecurityConfig.java
@@ -60,7 +60,6 @@ public class WebSecurityConfig {
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
                 .authorizeRequests().antMatchers("/api/auth/**").permitAll()
                 .antMatchers("/api/test/**").permitAll()
-                .antMatchers("/**").permitAll()
                 .anyRequest().authenticated();
 
         http.authenticationProvider(authenticationProvider());
